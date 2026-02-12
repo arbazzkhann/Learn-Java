ebcc308fbc6a6caff87b5c62f65f84a322e8f89d
Arbaz Khan <arbazkha78az@gmail.com>
2026-02-12 21:20:49 +0530

    Jagged Array in Java

---
+++ 3. Arrays/JaggedArray.java
@@ -0,0 +1,21 @@
+public class JaggedArray {
+    public static void main(String[] args) {
+
+        int[][] jagged = new int[3][];
+
+        jagged[0] = new int[2];  // 2 columns
+        jagged[1] = new int[4];  // 4 columns
+        jagged[2] = new int[3];  // 3 columns
+
+        jagged[0][0] = 1;
+        jagged[1][2] = 5;
+        jagged[2][1] = 9;
+
+        for(int i = 0; i < jagged.length; i++) {
+            for(int j = 0; j < jagged[i].length; j++) {
+                System.out.print(jagged[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
