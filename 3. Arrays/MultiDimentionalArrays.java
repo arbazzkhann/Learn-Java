3e08877b444a72d942b3b8388685d96ab30ba951
Arbaz Khan <arbazkha78az@gmail.com>
2026-02-12 20:21:02 +0530

    MultiDimentionalArray

---
+++ 3. Arrays/MultiDimentionalArrays.java
@@ -0,0 +1,11 @@
+public class MultiDimentionalArrays {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {22, 33, 21}, 
+            {1, 3, 4}, 
+            {22, 34, 3}
+        };
+
+        System.out.println(matrix[1][2]); //4
+    }
+}
