34f23e826975dc58369f3290c0125a8979e972ef
Arbaz Khan <arbazkha78az@gmail.com>
2026-02-14 19:17:43 +0530

    Enhanced for loop

---
+++ 5. Enhanced for Loop/Enhanced_for_Loop.java
@@ -0,0 +1,10 @@
+public class Enhanced_for_Loop {
+    public static void main(String[] args) {
+        int nums[] = {2,1,123,12,31,23,12,321,4,23,1};
+        
+        //enhanced for loop
+        for(int n: nums) {
+            System.out.println(n);
+        }
+    }
+}
